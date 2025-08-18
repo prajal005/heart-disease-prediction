0633e56c7c494ef8f7cf80187e7a559ba80e0a90
Prajal Irungbam <prajalirungbam@gmail.com>
2025-08-18 21:56:11 +0530

    Updated model_utils.py for hyper tuning.

--- scripts/model_utils.py
+++ scripts/model_utils.py
@@ -2,6 +2,8 @@
 
 import joblib
 import os
+import json
+import pandas as pd
 
 def save_model(model, filepath):
     """
@@ -38,3 +40,23 @@ def load_model(filepath):
     except Exception as e:
         print(f"Error loading model from {filepath}: {e}")
         return None
+    
+def save_tuning_results(results, best_params, filepath):
+    """
+    Saves GridSearchCV/RandomizedSearchCV results and best parameters to a JSON file.
+    """
+    try:
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
+        results_df = pd.DataFrame(results).sort_values(by= 'rank_test_score')
+
+        # Creating a Dictionary to save
+        output_data= {
+            'best parameters': best_params,
+            'results': results_df.to_dict(orient='records')
+        }
+
+        with open(filepath, 'w') as f:
+            json.dump(output_data, f, indent=4)
+        print(f"Tuning results saved to {filepath}")
+    except Exception as e:
+        print(f"Error saving tuning results: {e}")
